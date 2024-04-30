b80aa2703c47b3cbde2fb90027051b7ed8511449
Tiago Posse <tiagoposse@gmail.com>
2024-04-30 14:37:53 +0200

    start

--- internal/security/handler.go
+++ internal/security/handler.go
@@ -9,40 +9,39 @@ import (
 	"github.com/tiagoposse/ogent-auth/authorization"
 )
 
-
 func NewAuthorizationHandler(val authorization.IAuthTokenValidator, scopes map[string]authz.Scopes) *authorization.AuthorizationHandler {
 	ctrl := authz.NewAuthzController(scopes)
 
-  return &authorization.AuthorizationHandler{
-    AuthzController: ctrl,
-    IAuthTokenValidator: val,
-  }
+	return &authorization.AuthorizationHandler{
+		AuthzController:     ctrl,
+		IAuthTokenValidator: val,
+	}
 }
 
 func (h *authorization.AuthorizationHandler) HandleApiKeyAuth(c context.Context, operationName string, t ogent.ApiKeyAuth) (context.Context, error) {
-  session, err := h.ValidateApiKeyAuth(c, t.GetAPIKey())
+	session, err := h.ValidateApiKeyAuth(c, t.GetAPIKey())
 	if err != nil {
 		return c, err
 	}
 
-  ctx := context.WithValue(c, sessions.ContextSessionKey{}, session)
-  if h.ValidateScopes(ctx, operationName, session.GetScopes()); err != nil {
-    return ctx, nil
-  }
+	ctx := context.WithValue(c, sessions.ContextSessionKey{}, session)
+	if h.ValidateScopes(ctx, operationName, session.GetScopes()); err != nil {
+		return ctx, nil
+	}
 
 	return ctx, nil
 }
 
 func (h *authorization.AuthorizationHandler) HandleCookieAuth(c context.Context, operationName string, t ogent.ApiKeyAuth) (context.Context, error) {
-  session, err := h.ValidateCookieAuth(c, t.GetAPIKey())
+	session, err := h.ValidateCookieAuth(c, t.GetAPIKey())
 	if err != nil {
 		return c, err
 	}
 
-  ctx := context.WithValue(c, sessions.ContextSessionKey{}, session)
-  if h.ValidateScopes(ctx, operationName, session.GetScopes()); err != nil {
-    return ctx, nil
-  }
+	ctx := context.WithValue(c, sessions.ContextSessionKey{}, session)
+	if h.ValidateScopes(ctx, operationName, session.GetScopes()); err != nil {
+		return ctx, nil
+	}
 
 	return ctx, nil
 }
